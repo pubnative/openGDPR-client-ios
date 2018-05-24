d5c50acf4e722516818448baeb3c86c2871eb63c
Can Soykarafakılı <can.soykarafakili@gmail.com>
2018-05-24 14:50:27 +0200

    IdentityType is created

---
+++ PNOpenGDPRClient/PNOpenGDPRClient/Model/IdentityType.m
@@ -0,0 +1,39 @@
+//
+//  Copyright © 2018 PubNative. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import "IdentityType.h"
+
+@implementation IdentityType
+
++ (NSString *)controllerCustomerID {return @"controller_customer_id";}
++ (NSString *)androidAdvertisingID {return @"android_advertising_id";}
++ (NSString *)androidID {return @"android_id";}
++ (NSString *)email {return @"email";}
++ (NSString *)fireAdvertisingID {return @"fire_advertising_id";}
++ (NSString *)iOSAdvertisingID {return @"ios_advertising_id";}
++ (NSString *)iOSVendorID {return @"ios_vendor_id";}
++ (NSString *)microsoftAdvertisingID {return @"microsoft_advertising_id";}
++ (NSString *)microsoftPublisherID {return @"microsoft_publisher_id";}
++ (NSString *)rokuPublisherID {return @"roku_publisher_id";}
++ (NSString *)rokuAdvertisingID {return @"roku_advertising_id";}
+
+@end
